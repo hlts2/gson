4a17977bcab52d6d5d7cc63a12b4b381d242e1f3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-03-07 01:51:16 +0900

    add test case

--- gson_test.go
+++ gson_test.go
@@ -7,12 +7,13 @@ import (
 )
 
 type TestData struct {
-	json    string
-	isGoson bool
-	path    string
-	keys    []string
-	result  *Result
-	isErr   bool
+	json       string
+	isGoson    bool
+	path       string
+	keys       []string
+	result     *Result
+	objectType reflect.Kind
+	isErr      bool
 }
 
 type TestDatas []TestData
@@ -49,6 +50,42 @@ func (n TestData) CheckResultObject(result *Result) bool {
 		return true
 	}
 	return false
+}
+
+func (n TestData) CheckResultConvert() error {
+	var err error
+
+	switch n.objectType {
+	case reflect.Uint8:
+		_, err = n.result.Uint8()
+	case reflect.Uint16:
+		_, err = n.result.Uint16()
+	case reflect.Uint32:
+		_, err = n.result.Uint32()
+	case reflect.Uint64:
+		_, err = n.result.Uint64()
+	case reflect.Int8:
+		_, err = n.result.Int8()
+	case reflect.Int16:
+		_, err = n.result.Int16()
+	case reflect.Int32:
+		_, err = n.result.Int32()
+	case reflect.Int64:
+		_, err = n.result.Int64()
+	case reflect.Int:
+		_, err = n.result.Int()
+	case reflect.Float32:
+		_, err = n.result.Float32()
+	case reflect.Float64:
+		_, err = n.result.Float64()
+	case reflect.Complex64:
+		_, err = n.result.Complex64()
+	case reflect.Complex128:
+		_, err = n.result.Complex128()
+	case reflect.String:
+		_, err = n.result.String()
+	}
+	return err
 }
 
 var newTests = TestDatas{
@@ -165,7 +202,7 @@ var searchTests = TestDatas{
 		json:   `[{"name": "hlts2"}]`,
 		keys:   []string{"10"},
 		path:   "/10",
-		result: &Result{nil},
+		result: nil,
 		isErr:  true,
 	},
 	TestData{
@@ -205,7 +242,7 @@ var searchTests = TestDatas{
 		`,
 		keys:   []string{"friends", "100", "name"},
 		path:   "/friends/100/name",
-		result: &Result{nil},
+		result: nil,
 		isErr:  true,
 	},
 }
@@ -221,7 +258,7 @@ func TestSearch(t *testing.T) {
 		}
 
 		if !test.CheckResultObject(result) {
-			t.Error("")
+
 		}
 	}
 }
@@ -243,8 +280,89 @@ func TestPath(t *testing.T) {
 }
 
 var resultTests = TestDatas{
-	TestData{},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Uint8,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Uint16,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Uint32,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Uint64,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Int8,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Int16,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Int32,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Int64,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{10},
+		objectType: reflect.Int,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{1.0},
+		objectType: reflect.Float32,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{1.0},
+		objectType: reflect.Float64,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{1 + 0i},
+		objectType: reflect.Complex64,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{1 + 0i},
+		objectType: reflect.Complex128,
+		isErr:      false,
+	},
+	TestData{
+		result:     &Result{"hlts2"},
+		objectType: reflect.String,
+		isErr:      false,
+	},
 }
 
 func TestResult(t *testing.T) {
+
+	/*
+		var err error
+
+		for _, test := range resultTests {
+			err = test.CheckResultConvert()
+
+			if !test.CheckError(err) {
+				t.Error("")
+			}
+		}
+	*/
 }
